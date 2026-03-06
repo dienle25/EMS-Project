740db503825233fb226bbec4770f1a477a8d8dda
TranKhoaDang <dangtk.ce190256@gmail.com>
2026-03-06 15:49:53 +0700

    Update Task1_Linear_Regression.py

--- models/Task1_Linear_Regression.py
+++ models/Task1_Linear_Regression.py
@@ -4,34 +4,46 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
-# Đường dẫn tới file data
+# đường dẫn file
 data_path = os.path.join(os.path.dirname(__file__), '..', 'data', '04_Normalized_Data.csv')
 
 print("Loading file:", data_path)
 
-# Đọc dữ liệu
+# đọc data
 data = pd.read_csv(data_path)
 
 print("Dataset shape:", data.shape)
 print(data.head())
 
-# Giả sử cột cuối là target
-X = data.iloc[:, :-1]
-y = data.iloc[:, -1]
+# bỏ student_id vì không cần
+if "student_id" in data.columns:
+    data = data.drop("student_id", axis=1)
 
-# Chia train test
+# convert text -> number
+data = pd.get_dummies(data)
+
+print("\nAfter Encoding:")
+print(data.head())
+
+# tách X và y
+X = data.drop("exam_score", axis=1)
+y = data["exam_score"]
+
+# chia train test
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Train model
+# tạo model
 model = LinearRegression()
+
+# train
 model.fit(X_train, y_train)
 
-# Predict
+# predict
 y_pred = model.predict(X_test)
 
-# Evaluation
+# đánh giá
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
